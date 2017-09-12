a7934bdc3e3ce6ab2170b8ff017edf4aeeae421f
Chen Lu <chen@jwplayer.com>
2017-09-12 15:36:30 -0400

    htable

--- htable.py
+++ htable.py
@@ -5,6 +5,7 @@ Each bucket is a list of (key,value) tuples
 
 def htable(nbuckets):
     """Return a list of nbuckets lists"""
+    return [[] for x in xrange(5)]
 
 
 def hashcode(o):
@@ -14,14 +15,27 @@ def hashcode(o):
     For strings, perform operation h = h*31 + ord(c) for all characters in the string
     """
 
+    if type(o) is int:
+        return o
+    else if type(o) is str:
+        h = 0
+        for c in o:
+            h += h*31 + ord(c)
+    else
+        return None
 
-def bucket_indexof(table, key):
+
+def bucket_indexof(bucket, key):
     """
     You don't have to implement this, but I found it to be a handy function.
     Return the index of the element within a specific bucket; the bucket is:
     table[hashcode(key) % len(table)]. You have to linearly
     search the bucket to find the tuple containing key.
     """
+    for idx, val in enumerate(bucket):
+        if val == key:
+            return idx
+    return None
 
 
 def htable_put(table, key, value):
@@ -35,6 +49,13 @@ def htable_put(table, key, value):
     The type(value) can be anything. Could be a set, list, number, string, anything!
     """
 
+    bucket = table[hashcode(key) % len(table)]
+
+    b_index = bucket_indexof(bucket, key)
+    if b_index is None:
+        bucket.append((key, value))
+    else:
+        bucket[b_index] = (key, value)
 
 def htable_get(table, key):
     """
@@ -43,6 +64,15 @@ def htable_get(table, key):
     association with the key. Return the value (not the key and not
     the association!). Return None if key not found.
     """
+
+    bucket = table[hashcode(key) % len(table)]
+    b_index = bucket_indexof(bucket, key)
+
+    result = None
+    if b_index is not None:
+        result = (bucket[b_index])[1]
+    return result
+
 
 
 def htable_buckets_str(table):
@@ -56,7 +86,17 @@ def htable_buckets_str(table):
         0004->
     where parrt:99 indicates an association of (parrt,99) in bucket 3.
     """
-
+    output = ""
+    for index, bucket in enumerate(table):
+        output += str(index).zfill(4) + "->"
+        for entry in bucket:
+            data = list(entry)
+            output += data[0] + ":" + data[1] + ","
+        #clear extra ,
+        if output[-1] == ',':
+            output = output[:-1]
+        output += "\n"
+    return output
 
 def htable_str(table):
     """
@@ -65,3 +105,14 @@ def htable_str(table):
     insertion order within each bucket. The insertion order is
     guaranteed when you append to the buckets in htable_put().
     """
+
+    output = "{"
+    for bucket in table:
+        for entry in bucket:
+            data = list(entry)
+            output += data[0] + ":" + data[1] + ","
+    #clear extra ,
+    if output[-1] == ',':
+        output = output[:-1]
+    output += "}"
+    return output
